021bc47d71eb4a66b303d475d07544beacbfa137
Jonathan Kulak <lackadaize@msn.com>
2023-11-25 10:50:46 -0500

    This is the git commit for the Control Flow Lab assignment.

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Scuber if elseif and else function
+function scuberGreetingForFeet(distance) {
+
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(1500));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
+
+// Scuber ternary operator function
+function ternaryCheckCity(city) {
+  let isNYC = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return isNYC;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity("NYC"));
+console.log(ternaryCheckCity("Pittsburgh"));
+
+// Scuber switch function
+function switchOnCharmFromTip(tip) {
+
+  let isGoodTipper;
+
+  switch (tip) {
+    case 'generous':
+      isGoodTipper = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      isGoodTipper = 'Thank you.'
+      break;
+    default:
+      isGoodTipper = 'Bye.'
+      break;
+  }
+
+  return isGoodTipper;
 }
+
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('thanks for everything'));
